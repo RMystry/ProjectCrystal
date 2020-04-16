33924773c85f986090b57f8b93da6b7698fd28f7
Rishabh Mistry <rishabh.mistry@gmail.com>
2020-04-16 17:44:15 -0400

    jumper and sentinal base code set up

---
+++ ProjectCrystal/Assets/Scripts/Enemies/sentinal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sentinal : Enemy
+{
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+}
