a7d0dc060fdd923d083b6cae13b7293fad635027
mrichardsnh <36209350+mrichardsnh@users.noreply.github.com>
2020-05-07 16:26:15 -0400

    Small changes

--- ProjectCrystal/Assets/Scripts/Level/SpawnPoint.cs
+++ ProjectCrystal/Assets/Scripts/Level/SpawnPoint.cs
@@ -7,6 +7,7 @@ public class SpawnPoint : MonoBehaviour
     // Start is called before the first frame update
     public Crawler1 crawler;
     public sentinal sentry;
+    public Jumper jumper;
 
     void Start()
     {
@@ -19,8 +20,25 @@ public class SpawnPoint : MonoBehaviour
         
     }
 
-    public void SpawnEnemies()
+    public void SpawnEnemy()
     {
+        //Create random integer to determine what enemy to spawn
+        int random = (int) (Random.Range(1, 3) + .5f);
 
+        switch(random)
+        {
+            case 1:
+                Instantiate(crawler, this.transform.position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(sentry, this.transform.position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(jumper, this.transform.position, Quaternion.identity);
+                break;
+            default:
+                break;
+
+        }
     }
 }
