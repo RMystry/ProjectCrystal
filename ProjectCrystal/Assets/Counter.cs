aa0375465fffa32f08cdac504a78eca0eb18b20d
mrichardsnh <36209350+mrichardsnh@users.noreply.github.com>
2020-05-07 15:07:01 -0400

    Moving between rooms

---
+++ ProjectCrystal/Assets/Counter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    int currentRoom;
+    int numOfEnemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentRoom = 0;
+        numOfEnemies = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int getCurrentRoom()
+    {
+        return currentRoom;
+    }
+
+    public void setCurrentRoom(int num)
+    {
+        currentRoom = num;
+    }
+
+    public void setNumOfEnemies(int num)
+    {
+        numOfEnemies = num;
+    }
+
+    public int getNumOfEnemies()
+    {
+        return numOfEnemies;
+    }
+}
